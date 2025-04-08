48a97b330066517bc98cde398cd50bd62679e5dc
marc4919 <marcgeek4919@gmail.com>
2025-04-08 21:51:27 +0200

    Initial Commit

---
+++ CookingTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookingTimer
+//
+//  Created by Marc Garcia Teodoro on 8/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookingTimer/CookingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingTimerApp.swift
+//  CookingTimer
+//
+//  Created by Marc Garcia Teodoro on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CookingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
