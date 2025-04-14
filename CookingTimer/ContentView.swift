1f76537e0285d6c68853ff092f18ea0eefbdb4df
marc4919 <marcgeek4919@gmail.com>
2025-04-14 18:29:24 +0200

    style: force black text in Picker.wheel due to dark mode issues

--- CookingTimer/ContentView.swift
+++ CookingTimer/ContentView.swift
@@ -38,13 +38,19 @@ struct ContentView: View {
                     HStack {
                         Picker("minutes", selection: $minutes) {
                             ForEach(0...59, id: \.self) { index in
-                                Text("\(index)").tag(index).font(.title)
+                                Text("\(index)")
+                                    .tag(index)
+                                    .font(.title)
+                                    .foregroundStyle(.black)
                             }
                         }.pickerStyle(.wheel).frame(width: 160, height: 160)
                             .disabled(isTimerRunning)
                         Picker("seconds", selection: $seconds) {
                             ForEach(0...59, id: \.self) { index in
-                                Text("\(index)").tag(index).font(.title)
+                                Text("\(index)")
+                                    .tag(index)
+                                    .font(.title)
+                                    .foregroundStyle(.black)
                             }
                         }.pickerStyle(.wheel).frame(width: 160, height: 160)
                             .disabled(isTimerRunning)
